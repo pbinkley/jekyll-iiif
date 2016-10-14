b37a468a889d6f5b63a589d647a779ead4733e83
Peter Binkley <peter.binkley@ualberta.ca>
2016-10-13 18:12:25 -0600

    Allow multiple instances with distinct ids

--- lib/iiif.rb
+++ lib/iiif.rb
@@ -1,4 +1,5 @@
 class IIIFTag < Liquid::Tag
+  @@instance = 0
   def initialize(tag_name, image, tokens)
     super
     @image = image.strip
@@ -15,21 +16,41 @@ class IIIFTag < Liquid::Tag
   end
 
   def render(context)
-    <<-MARKUP.strip
+    @@instance += 1
+    if @@instance == 1
+      topper = <<-TOPPER.strip 
 <script src="osd/openseadragon.min.js"></script>
-<div id="openseadragon1"></div>  
 <script>
 //<![CDATA[
-        function initOpenSeadragon() {
-          OpenSeadragon({
-  id: "openseadragon1",
-  minZoomImageRatio: 1,
-  prefixUrl: "osd/images/",
-  tileSources: "tiles\/#{ lookup(context, @image) }/info.json",
-  crossOriginPolicy: false});
-        }
-        window.onload = initOpenSeadragon;
-        document.addEventListener("page:load", initOpenSeadragon); // Initialize when using turbolinks
+var osdfuncs = [];
+window.onload = function() {
+  var arrayLength = osdfuncs.length;
+  for (var i = 0; i < arrayLength; i++) {
+    osdfuncs[i]();
+  }
+}
+//]]
+</script>
+      TOPPER
+    else
+      topper = ""
+    end
+    <<-MARKUP.strip
+    #{ topper }
+<div id="openseadragon#{ @@instance }" class="openseadragon"></div>  
+<script>
+//<![CDATA[
+osdfuncs.push(
+  function initOpenSeadragon#{ @@instance }() {
+    OpenSeadragon({
+      id: "openseadragon#{ @@instance }",
+      minZoomImageRatio: 1,
+      prefixUrl: "osd/images/",
+      tileSources: "tiles\/#{ lookup(context, @image) }/info.json",
+      crossOriginPolicy: false
+    });
+  }
+)
 //]]>
 </script>
     MARKUP
