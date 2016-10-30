6cf59c21478fd25c97ddac7d9a374c894bab561e
Peter Binkley <peter.binkley@ualberta.ca>
2016-10-30 11:20:00 -0600

    Refactor gallery to use iiif_s3 thumbnail, show title

--- lib/iiif_gallery.rb
+++ lib/iiif_gallery.rb
@@ -6,14 +6,16 @@ class IIIFGalleryTag < IIIF
 
   def render(context)
     images = ""
-    imagefiles = Dir["_iiif/*"].sort!
-    imagefiles.each do |image|
+    coll = context.registers[:site].collections["iiif_collection"]
+    coll.docs.each do |image|
+      Jekyll.logger.info("IIIF:", image.path)
       # image has "_iiif/" prefix, which must be removed
-      basename = File.basename(image, ".*")
+      basename = File.basename(image.path, ".*")
       # get thumbnail link from tiles/<basename>/manifest.json
       manifest = JSON.parse(File.read("./tiles/" + basename + "/manifest.json"))
       thumbnail = manifest["thumbnail"]
       context.registers[:page]["thumbnail"] = thumbnail
+      context.registers[:page]["thistitle"] = image.data["title"]
       images += render_instance(basename, "iiif_thumbnail", context)
     end
     images
