30f9528f753e15a7e7b0d60929b28a93e8cf64a5
Peter Binkley <peter.binkley@ualberta.ca>
2016-10-29 21:08:26 -0600

    Replace thumbnail IIIF instances with proper thumbnail images

--- lib/iiif_gallery.rb
+++ lib/iiif_gallery.rb
@@ -5,16 +5,19 @@ class IIIFGalleryTag < IIIF
   end
 
   def render(context)
-    imagefiles = Dir["_iiif/*"].sort!
     images = ""
+    imagefiles = Dir["_iiif/*"].sort!
     imagefiles.each do |image|
       # image has "_iiif/" prefix, which must be removed
       basename = File.basename(image, ".*")
+      # get thumbnail link from tiles/<basename>/manifest.json
+      manifest = JSON.parse(File.read("./tiles/" + basename + "/manifest.json"))
+      thumbnail = manifest["thumbnail"]
+      context.registers[:page]["thumbnail"] = thumbnail
       images += render_instance(basename, "iiif_thumbnail", context)
     end
     images
   end
-
 end
 
 Liquid::Template.register_tag('iiif_gallery', IIIFGalleryTag)
