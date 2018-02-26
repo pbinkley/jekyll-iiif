f8ed55f870a6bb695fb060e09ea2422f397d59fd
Peter Binkley <peter.binkley@ualberta.ca>
2018-02-25 21:39:15 -0700

    Standardize dev urls as localhost, not 127.0.0.1

--- lib/iiif_url.rb
+++ lib/iiif_url.rb
@@ -11,13 +11,12 @@ end
 Liquid::Template.register_filter(Jekyll::IIIFUrlFilter)
 
 Jekyll::Hooks.register :site, :after_init do |site|
- # site = @context.registers[:site]
-  # if iiifurl is explicit in config, leave it along
+  # if iiifurl is explicit in config, leave it alone
   break if site.config['iiifurl']
   site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
   if site.config['env'] == 'production'
     site.config['iiifurl'] = site.config['url']
   else
-    site.config['iiifurl'] = 'http://127.0.0.1:' + site.config['port']
+    site.config['iiifurl'] = 'http://localhost:' + site.config['port']
   end
 end
